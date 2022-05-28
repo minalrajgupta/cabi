166fcb4bf20c1f48cd515718cb6e3d272adca7dc
Minal Raj Gupta <minal@krishaweb.net>
2022-05-28 22:51:02 +0530

    Custom post type name News created

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -15,3 +15,34 @@ function twentytwenty_child_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'twentytwenty_child_enqueue_styles' );
+
+/**
+ * Creating Custom Post type : News
+ */
+function twentytwenty_child_custom_post_news() {
+    $labels = array(
+        'name'               => _x( 'News', 'twentytwenty' ),
+        'singular_name'      => _x( 'News', 'twentytwenty' ),
+        'add_new'            => _x( 'Add News', 'twentytwenty' ),
+        'add_new_item'       => __( 'Add New News', 'twentytwenty'),
+        'edit_item'          => __( 'Edit News', 'twentytwenty' ),
+        'new_item'           => __( 'New News', 'twentytwenty' ),
+        'all_items'          => __( 'All News', 'twentytwenty' ),
+        'view_item'          => __( 'View News', 'twentytwenty' ),
+        'search_items'       => __( 'Search News', 'twentytwenty' ),
+        'not_found'          => __( 'No News found', 'twentytwenty' ),
+        'not_found_in_trash' => __( 'No News found in the Trash', 'twentytwenty' ),
+        'parent_item_colon'  => __( 'Parent News', 'twentytwenty' ),
+        'menu_name'          => 'News'
+    );
+    $args = array(
+        'labels'        => $labels,
+        'description'   => 'Holds our news and news specific data',
+        'public'        => true,
+        'menu_position' => 4,
+        'supports'      => array( 'title', 'editor', 'thumbnail' ),
+        'has_archive'   => true,
+    );
+    register_post_type( 'news', $args );
+}
+add_action( 'init', 'twentytwenty_child_custom_post_news' );
